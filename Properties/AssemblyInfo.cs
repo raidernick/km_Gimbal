3aa0092a7521e9ca6748e3aca013e505da888806
Sarbian <Sarbian@sarbian.com>
2014-12-27 19:03:02 +0100

    v3.0.1.0 + version info file

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("KM_Gimbal")]
+[assembly: AssemblyProduct("KM_Gimbal")]
+[assembly: AssemblyCopyright("Dtobi & Sarbian")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
